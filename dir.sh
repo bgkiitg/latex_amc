05ab7b26084c2aec3bddb598f20601a10b17ae68
Benny George Kenkireth <ben@iitg.ac.in>
2024-02-24 14:54:45 +0530

    Added the xml sources and dir.sh

---
+++ dir.sh
@@ -0,0 +1,8 @@
+# This program may be run to create a directory structure suitable for use with AMC
+#!/bin/bash
+mkdir -p cr
+mkdir -p cr/corrections
+mkdir -p cr/corrections/pdf
+mkdir -p cr/corrections/jpg
+mkdir -p diagnostic
+mkdir -p zooms
